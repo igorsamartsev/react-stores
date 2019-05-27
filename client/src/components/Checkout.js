5e21e866ffb19a2105ac4301ddd5d3a8bdfe78c2
Igor Samartsev <samartsev@sibers.com>
2019-05-27 20:18:51 +0600

    add header

---
+++ client/src/components/Checkout.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+class Checkout extends Component{
+  render(){
+    return (
+      <div>
+        Checkout
+      </div>
+    )
+  }
+}
+
+
+export default Checkout;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Checkout from "./components/Checkout";
+import Navbar from "./components/Navbar";
+import 'gestalt/dist/gestalt.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Root = () =>(
+
+  <Router>
+    <React.Fragment>
+    <Navbar/>
+    <Switch>
+    <Route path='/' exact component={App}/>
+    <Route path='/signin' component={Signin}/>
+    <Route path='/signup' component={Signup}/>
+    <Route path='/checkout' component={Checkout}/>
+    </Switch>
+    </React.Fragment>
+  </Router>
+
+
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
